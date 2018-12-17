1b4e1874f6ef35c8f2a52e0818354f31936aa8e0
Wilson <wilsoncampusanojorge@gmail.com>
2018-12-17 10:19:35 -0400

    bezier path

--- WorldTrackingUd/World Tracking/ViewController.swift
+++ WorldTrackingUd/World Tracking/ViewController.swift
@@ -21,18 +21,23 @@ class ViewController: UIViewController {
     }
     @IBAction func add(_ sender: UIButton) {
         let node = SCNNode()
-        let cilinderWithCode = SCNCone(topRadius: 0.3, bottomRadius: 0.3, height: 0.5)
+        //let cilinderWithCode = SCNCone(topRadius: 0.3, bottomRadius: 0.3, height: 0.5)
+        let sphere = SCNSphere(radius: 0.1)
         
-        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0.0))
+        path.addLine(to: CGPoint(x: 0, y: 0.2))
+        path.addLine(to: CGPoint(x: 0.2, y: 0.3))
+        let shape = SCNShape(path: path, extrusionDepth: 0.05)
         //node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
-        node.geometry = cilinderWithCode
+        node.geometry = shape
         node.geometry?.firstMaterial?.specular.contents = UIColor.orange
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.black
         
         let x = Util.randomNumbers(firstNum: -0.3, secondNum: 0.3)
         let y = Util.randomNumbers(firstNum: -0.3, secondNum: 0.3)
         let z = Util.randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        node.position = SCNVector3(x,y,z)
+        node.position = SCNVector3(0,0,-0.5)
         self.scenView.scene.rootNode.addChildNode(node)
     }
     @IBAction func reiniciar(_ sender: Any) {
