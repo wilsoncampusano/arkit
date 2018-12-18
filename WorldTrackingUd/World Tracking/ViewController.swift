9b213dee27f64942b98266bb814976c4086293a5
Wilson <wilsoncampusanojorge@gmail.com>
2018-12-18 14:40:23 -0400

    techo de la casa

--- WorldTrackingUd/World Tracking/ViewController.swift
+++ WorldTrackingUd/World Tracking/ViewController.swift
@@ -20,27 +20,19 @@ class ViewController: UIViewController {
         scenView.session.run(config)
     }
     @IBAction func add(_ sender: UIButton) {
-        let node = SCNNode()
-        //let cilinderWithCode = SCNCone(topRadius: 0.3, bottomRadius: 0.3, height: 0.5)
-        _ = SCNSphere(radius: 0.1)
+        let boxNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.0))
+        let triangleNode = SCNNode(geometry: SCNPyramid(width: 0.1, height: 0.1, length: 0.1))
+        boxNode.geometry?.firstMaterial?.specular.contents = UIColor.orange
+        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.black
+        boxNode.position = SCNVector3(0.2,0.3,-0.20)
         
-        let path = UIBezierPath()
-        path.move(to: CGPoint(x: 0, y: 0.0))
-        path.addLine(to: CGPoint(x: 0, y: 0.2))
-        path.addLine(to: CGPoint(x: 0.2, y: 0.3))
-        path.addLine(to: CGPoint(x:0.4, y:0.2))
-        path.addLine(to: CGPoint(x: 0.4, y: 0.0))
-        let shape = SCNShape(path: path, extrusionDepth: 0.05)
-        //node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
-        node.geometry = shape
-        node.geometry?.firstMaterial?.specular.contents = UIColor.orange
-        node.geometry?.firstMaterial?.diffuse.contents = UIColor.black
+        triangleNode.geometry?.firstMaterial?.specular.contents = UIColor.green
+        triangleNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        triangleNode.position = SCNVector3(0, 0.05, 0)
         
-        _ = Util.randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        _ = Util.randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        _ = Util.randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        node.position = SCNVector3(0,0,-0.1)
-        self.scenView.scene.rootNode.addChildNode(node)
+        boxNode.addChildNode(triangleNode)
+        
+        self.scenView.scene.rootNode.addChildNode(boxNode)
     }
     @IBAction func reiniciar(_ sender: Any) {
         self.resset()
