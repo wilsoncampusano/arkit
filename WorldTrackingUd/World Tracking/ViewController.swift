08c01a7b0eace8f04d1348283f20ff3a8c90682b
Wilson <wilsoncampusanojorge@gmail.com>
2019-01-07 10:17:20 -0400

    rotando la casa

--- WorldTrackingUd/World Tracking/ViewController.swift
+++ WorldTrackingUd/World Tracking/ViewController.swift
@@ -20,37 +20,38 @@ class ViewController: UIViewController {
         scenView.session.run(config)
     }
     @IBAction func add(_ sender: UIButton) {
-//        let boxNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.0))
-//        let triangleNode = SCNNode(geometry: SCNPyramid(width: 0.1, height: 0.1, length: 0.1))
-//        let planeNode = SCNNode(geometry: SCNPlane(width: 0.04, height: 0.06))
-//
-//        boxNode.geometry?.firstMaterial?.specular.contents = UIColor.orange
-//        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.black
-//        boxNode.position = SCNVector3(0.2,0.3,-0.20)
-//
-//        triangleNode.geometry?.firstMaterial?.specular.contents = UIColor.green
-//        triangleNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
-//        triangleNode.position = SCNVector3(0, 0.05, 0)
-//
-//        planeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
-//        planeNode.position = SCNVector3(x: 0, y: -0.02, z: 0.053)
-//
-//        boxNode.addChildNode(triangleNode)
-//        boxNode.addChildNode(planeNode)
-//        self.scenView.scene.rootNode.addChildNode(boxNode)
+        let boxNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.0))
+        let triangleNode = SCNNode(geometry: SCNPyramid(width: 0.1, height: 0.1, length: 0.1))
+        let planeNode = SCNNode(geometry: SCNPlane(width: 0.04, height: 0.06))
+
+        boxNode.geometry?.firstMaterial?.specular.contents = UIColor.orange
+        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.black
+        boxNode.position = SCNVector3(0.2,0.3,-0.20)
+
+        triangleNode.geometry?.firstMaterial?.specular.contents = UIColor.green
+        triangleNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        triangleNode.position = SCNVector3(0, 0.05, 0)
+
+        planeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
+        planeNode.position = SCNVector3(x: 0, y: -0.02, z: 0.053)
+
+        boxNode.addChildNode(triangleNode)
+        boxNode.addChildNode(planeNode)
+        boxNode.eulerAngles = SCNVector3(x: 0, y: 0, z: Float(90.radians))
+        self.scenView.scene.rootNode.addChildNode(boxNode)
         
-        let planeNode = SCNNode(geometry: SCNPlane(width: 0.1, height: 0.1))
-        planeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        planeNode.position = SCNVector3(0.0,0.0,-0.1)
-        planeNode.eulerAngles = SCNVector3(0,0,90.radians)
+//        let planeNode = SCNNode(geometry: SCNPlane(width: 0.1, height: 0.1))
+//        planeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+//        planeNode.position = SCNVector3(0.0,0.0,-0.1)
+//        planeNode.eulerAngles = SCNVector3(0,0,90.radians)
+//
+//        let pyramidNode = SCNNode(geometry: SCNPyramid(width: 0.1, height: 0.1, length: 0.1))
+//        pyramidNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+//        pyramidNode.position = SCNVector3(0.0,0,-0.2)
         
-        let pyramidNode = SCNNode(geometry: SCNPyramid(width: 0.1, height: 0.1, length: 0.1))
-        pyramidNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
-        pyramidNode.position = SCNVector3(0.0,0,-0.2)
+      //  planeNode.addChildNode(pyramidNode)
         
-        planeNode.addChildNode(pyramidNode)
-        
-        self.scenView.scene.rootNode.addChildNode(planeNode)
+  //      self.scenView.scene.rootNode.addChildNode(planeNode)
         
     }
     @IBAction func reiniciar(_ sender: Any) {
