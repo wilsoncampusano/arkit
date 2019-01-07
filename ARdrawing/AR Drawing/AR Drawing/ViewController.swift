2cb3a1a55de84e14795baf01b333cdab4bd0cc51
Wilson <wilsoncampusanojorge@gmail.com>
2019-01-07 15:38:42 -0400

    obteniendo punto frente a la camara

--- ARdrawing/AR Drawing/AR Drawing/ViewController.swift
+++ ARdrawing/AR Drawing/AR Drawing/ViewController.swift
@@ -19,12 +19,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         self.sceneView.debugOptions = [.showWorldOrigin, .showFeaturePoints]
         self.sceneView.showsStatistics = true
-        self.sceneView.delegate = self 
+        self.sceneView.delegate = self
         self.sceneView.session.run(configuration)
     }
     
     func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
         print("rendering")
+        guard let pointOfView = renderer.pointOfView else {return }
+        
+        let transformation = pointOfView.transform
+        
+        let position = SCNVector3(-transformation.m31, -transformation.m32, -transformation.m33)
+        
+        let orientation = SCNVector3(transformation.m41, transformation.m42, transformation.m43)
+        
+        let frontOfCamera = orientation + position
+        
+        print("x = \(position.x) y = \(position.y) z =  \(position.z)")
+        
     }
+    
 }
 
+extension SCNVector3{
+    static func +(left:SCNVector3, right:SCNVector3) -> SCNVector3{
+        return SCNVector3Make(left.x + right.x, left.y+right.y, left.z+right.z)
+    }
+}
